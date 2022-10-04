539a5b350070f8a4a51d50d4198a8324db273f73
HailGanyu <henryyanguk@gmail.com>
2022-10-04 16:59:24 +0100

    added raycaster

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,27 @@ function App() {
     sphereGeometry.userData = { URL: "https://github.com/GanyuHail/3dArt/blob/main/Hi%20Res%20-.jpg" };
 
     const controls = new OrbitControls(camera, renderer.domElement);
-
+    const raycaster = new THREE.Raycaster();
+    const pointer = new THREE.Vector2();
+    
+    function onPointerMove( event ) {
+      pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+      pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    }
+    
+    function render() {
+      raycaster.setFromCamera( pointer, camera );
+    
+      const intersects = raycaster.intersectObjects( scene.children );
+      for ( let i = 0; i < intersects.length; i ++ ) {
+        intersects[ i ].window.open("www.google.com")
+      }
+      renderer.render( scene, camera );
+    }
+    
+    window.addEventListener( 'pointermove', onPointerMove );
+    
+    window.requestAnimationFrame(render);
     const animate = () => {
       sphereMesh.rotation.x += 0.001;
       sphereMesh.rotation.y += 0.001;
